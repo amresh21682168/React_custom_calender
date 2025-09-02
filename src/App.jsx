bf9b98f13d9c404271244e502cb7c7af624dbadd
Amresh <amresh.kumar@kreeti.com>
2025-09-02 15:49:54 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -80,8 +80,7 @@ const App = () => {
           {" "}
           +Add Events{" "}
         </button>
-        className="fixed bottom-6 right-6 bg-blue-600 text-white rounded-full
-        px-4 py-2 shadow-lg"
+        
         <EventModal
           isOpen={showModal}
           defaultDate={formatDate(currentDate)}
